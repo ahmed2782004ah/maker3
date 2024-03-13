af1e1b7a26d892e5a1f289e0f69ea58f08d15a88
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-13 22:41:23 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,10 +1,10 @@
 # متطلبات التنصيب #
 OWNER = ["K_O_W_5", "IIUll_l"]
 OWNER_NAME = "ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪"
-BOT_TOKEN = "6368773285:AAHTC8m6vX-6O4N7YfT27HQp0ZSQjWgdnZo"
+BOT_TOKEN = "6462080196:AAFAc2-oAzqT6S01twqv6Ra0GqN6GAcYQUE"
 DATABASE = "mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/UI_VM"
 GROUP = "https://t.me/YR_HX"
-VIDEO = "https://t.me/GGGTJ8t/2"
+VIDEO = "https://telegra.ph/file/0913f5246d0532e170e21.mp4"
 LOGS = "GGGTJ8t"
 
