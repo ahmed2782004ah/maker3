4784132a56d1ba30aa8c5c622b247baac42cd031
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-14 10:48:21 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["K_O_W_5", "IIUll_l"]
 OWNER_NAME = "ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪"
-BOT_TOKEN = "6462080196:AAFAc2-oAzqT6S01twqv6Ra0GqN6GAcYQUE"
+BOT_TOKEN = "6430322526:AAFXcmRTgreTlwMspO2KCqRY-33xuXIz-bs"
 DATABASE = "mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/UI_VM"
 GROUP = "https://t.me/YR_HX"
